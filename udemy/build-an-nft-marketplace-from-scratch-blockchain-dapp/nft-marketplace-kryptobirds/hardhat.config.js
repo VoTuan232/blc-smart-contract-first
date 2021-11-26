7bf0087a6e0bae720b56a1aca588aa00f1dc21ef
tuan.vo <tuan.vo@sotatek.com>
2021-11-26 21:54:44 +0700

    chore: update

--- udemy/build-an-nft-marketplace-from-scratch-blockchain-dapp/nft-marketplace-kryptobirds/hardhat.config.js
+++ udemy/build-an-nft-marketplace-from-scratch-blockchain-dapp/nft-marketplace-kryptobirds/hardhat.config.js
@@ -1,14 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
+const projectId = 'be1e3e21ae194245a8cd36dc19df5a30'
+const fs = require('fs')
+const keyData = fs.readFileSync('./p-key.txt', {
+  encoding:'utf8', flag:'r'
+})
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+console.log('[keyData]', keyData)
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,6 +13,28 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
+ module.exports = {
+  defaultNetwork: 'hardhat',
+  networks:{
+    hardhat:{
+      chainId: 1337 // config standard 
+    },
+    mumbai:{
+      url:`https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts:[keyData]
+    },
+    mainnet: {
+      url:`https://mainnet.infura.io/v3/${projectId}`,
+      accounts:[keyData]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
